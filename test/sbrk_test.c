e38b98f6c64d4aa7aa4a4260a52b71f50308c5cc
Bobompasato <mendechongtai@gmail.com>
2026-09-18 15:20:52 +0900

    test: edit sbrk test

--- test/sbrk_test.c
+++ test/sbrk_test.c
@@ -6,10 +6,14 @@ int main(void){
     void *a = sbrk(0);
     void *b = sbrk(15);
     void *c = sbrk(0);
+    void *d = sbrk(16);
+    void *e = sbrk(0);
 
     printf("a: %p\n", a);
     printf("b: %p\n", b);
     printf("c: %p\n", c);
+    printf("d: %p\n", d);
+    printf("e: %p\n", e);
 
     return 0;
 }
